46fdfcd86b794f6a0584603a38a0ee8e99638af9
Tato <gastomolina91@gmail.com>
2015-05-18 16:25:20 -0300

    Updated standUp show to only show the corresponding tasks and blockers

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,12 @@ Blocker.create!(title: 'First Blocker',   description: 'My first Block!',   stan
 Blocker.create!(title: 'Second Blocker',  description: 'My second Block!',  stand_up: firstStand);
 Task.create!(title: 'First Task',  description: 'My first Task!',  stand_up: firstStand);
 Task.create!(title: 'Second Task', description: 'My second Task!', stand_up: firstStand);
+yesterdayTask1 = Task.create!(title: 'First Task from Yesterday', description: 'My second Task from Yesterday!', stand_up: firstStand);
+yesterdayTask2 = Task.create!(title: 'Second Task from Yesterday', description: 'My second Task from Yesterday!', stand_up: firstStand);
+yesterdayBlocker = Blocker.create!(title: 'Blocker from Yesterday',  description: 'My Blocker from Yesterday!',  stand_up: firstStand);
+yesterdayTask1.created_at = DateTime.yesterday
+yesterdayTask2.created_at = DateTime.yesterday
+yesterdayBlocker.created_at = DateTime.yesterday
+yesterdayTask1.save!
+yesterdayTask2.save!
+yesterdayBlocker.save!
