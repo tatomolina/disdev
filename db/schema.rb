e676652c456b48a06fc95780e39693fd2427a0ee
Tato <gastomolina91@gmail.com>
2015-06-09 00:24:30 -0300

    removed work_group_id from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609024718) do
+ActiveRecord::Schema.define(version: 20150609032214) do
 
   create_table "blockers", force: true do |t|
     t.string   "title"
@@ -78,12 +78,10 @@ ActiveRecord::Schema.define(version: 20150609024718) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "work_group_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["work_group_id"], name: "index_users_on_work_group_id"
 
   create_table "users_roles", id: false, force: true do |t|
     t.integer "user_id"
