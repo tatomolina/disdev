ed7a2136997dbdbf878ecc68a01796e83ce17fff
Tato <gastomolina91@gmail.com>
2015-06-22 17:50:17 -0300

    Added reply to the show conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -16,11 +16,20 @@ class ConversationsController < ApplicationController
     conversation.mark_as_read(current_user)
   end
 
+  def reply
+    current_user.reply_to_conversation(conversation, message_params[:body])
+    flash[:notice] = "Your reply message was successfully sent!"
+    redirect_to conversation_path(conversation)
+  end
 
   private
 
   def conversation_params
     params.require(:conversation).permit(:subject, :body,recipients:[])
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :subject)
   end
 
 end
