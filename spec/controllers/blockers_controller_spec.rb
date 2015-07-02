a5c81dae6533b7ab46757c57ede46bbde429135a
Tato <gastomolina91@gmail.com>
2015-07-02 17:18:25 -0300

    Fixed Controller test

--- spec/controllers/blockers_controller_spec.rb
+++ spec/controllers/blockers_controller_spec.rb
@@ -7,26 +7,24 @@ RSpec.describe BlockersController, :type => :controller do
   }
 
   let(:blocker) do
-    blocker = Blocker.create!(title: "super blocker", description: "I'm a super blocker")
-    blocker.stand_up = stand_up
+    blocker = Blocker.create!(title: "super blocker", description: "I'm a super blocker",
+    stand_up: stand_up)
     blocker
   end
 
-  let(:stand_up) do
-    stand_up = StandUp.create!()
-    stand_up.project = project
-    stand_up.user = user
-    stand_up
-  end
+  let(:stand_up) { StandUp.create!(user: user, project: project) }
 
   let(:project) do
-    project = Project.create!(name: "Project")
-    project.work_group = group
-    project.add! user
-    project
+    project1 = Project.create!(name: "Project", work_group: group)
+    project1.add! user
+    project1
   end
 
-  let(:group) { WorkGroup.create!(name: "Group")}
+  let(:group) do
+     group1 = WorkGroup.create!(name: "Group")
+     group1.add! user
+     group1
+   end
 
 
 
@@ -34,7 +32,6 @@ RSpec.describe BlockersController, :type => :controller do
       sign_in user
     end
 
-=begin
   describe 'GET show' do
     it "returns 200 (ok) response code" do
       get :show, id: blocker
@@ -52,7 +49,6 @@ RSpec.describe BlockersController, :type => :controller do
     end
 
   end
-
   describe 'POST updte' do
     let(:attr) do
       { title: "I'm a title", description: "I'm a super duper description"}
@@ -62,39 +58,37 @@ RSpec.describe BlockersController, :type => :controller do
     end
     context "Valid attributes" do
       before(:each) do
-        @blocker = FactoryGirl.create(:blocker)
-        post :update, id: @blocker.id, :blocker => attr
-        @blocker.reload
+        post :update, id: blocker.id, :blocker => attr
+        blocker.reload
       end
       it "if the update is okay it should redirect to show" do
-        expect(response).to redirect_to(@blocker)
+        expect(response).to redirect_to(blocker)
       end
 
       it "if it update should change" do
-        expect(@blocker.title).to eql(attr[:title])
-        expect(@blocker.description).to eql(attr[:description])
+        expect(blocker.title).to eql(attr[:title])
+        expect(blocker.description).to eql(attr[:description])
       end
 
     end
 
     context "InValid attributes" do
       before(:each) do
-        @blocker = FactoryGirl.create(:blocker)
-        post :update, id: @blocker.id, :blocker => invalid_attr
-        @blocker.reload
+        post :update, id: blocker.id, :blocker => invalid_attr
+        blocker.reload
       end
 
-      it "locates the requested @blocker" do
-        assigns(:blocker).should eq(@blocker)
+      it "locates the requested blocker" do
+        assigns(:blocker).should eq(blocker)
       end
 
       it "if the update has any error should render edit" do
         expect(response).to render_template :edit
       end
 
-      it "does not change @blocker attributes" do
-        expect(@blocker.title).should_not eq(invalid_attr[:title])
-        expect(@blocker.description).should_not eq(invalid_attr[:description])
+      it "does not change blocker attributes" do
+        expect(blocker.title).should_not eq(invalid_attr[:title])
+        expect(blocker.description).should_not eq(invalid_attr[:description])
       end
 
     end
@@ -102,22 +96,19 @@ RSpec.describe BlockersController, :type => :controller do
 
 
   describe "DELETE destroy" do
-    before(:each) do
-      @blocker = FactoryGirl.create(:blocker)
-      @blocker.stand_up = stand_up
-    end
-
+    before(:each) {
+      blocker
+    }
     it "deletes the blocker" do
       expect{
-        delete :destroy, id: @blocker
+        delete :destroy, id: blocker
       }.to change(Blocker, :count).by(-1)
     end
 
     it "redirect to stand_up#show" do
-      delete :destroy, id: @blocker
+      delete :destroy, id: blocker
       response.should redirect_to stand_up_path(stand_up)
     end
 
   end
-=end
 end
